8137508eaf5118fbdef26ce0b524c5f8576dc56c
VanyaSay <46083400+VanyaSay@users.noreply.github.com>
2019-01-13 11:46:03 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-	$('.image-link').magnificPopup({type:'image'});
-  });
+
+(function($) {
+
+	$(document).ready(function($){
+   	$('.image-link').magnificPopup({type:'image'});
+ });
+
+
+})(jQuery);
+$( ".mobilemenu" ).on( "click", function() {
+	$('.header_links').toggleClass( "changed" );
+});
+$( ".mobilemenu" ).on( "click", function() {
+	$(this).toggleClass( "changed_menu" );
+	$(this).toggleClass( "mobilemenu" );
+});
